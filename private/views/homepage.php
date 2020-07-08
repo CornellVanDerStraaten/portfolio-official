eecf6eade1013555c9f1fa885eb45a415f5941bc
Cornell van der Straaten <30183@ma-web.nl>
2020-07-08 20:01:44 +0200

    Setup done to start working on homepage Mobile

--- private/views/homepage.php
+++ private/views/homepage.php
@@ -1,26 +1,17 @@
-<?php $this->layout('website');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	
+</body>
+</html>
+<h1 class="header-text">Web & Software Developer</h1>
 
-<?php $this->start('sidebar')?>
-<div class="top-10">
-	<h3>Top 10 Buren</h3>
-	<ol>
-		<li>Pieter</li>
-		<li>Achmed</li>
-		<li>Rosa</li>
-		<li>Alper</li>
-		<li>Daan</li>
-		<li>Sjoerd</li>
-		<li>Mo</li>
-		<li>Chantal</li>
-		<li>Floor</li>
-		<li>Duneya</li>
-	</ol>
-</div>
-<?php $this->stop()?>
-
-<h1>Welkom</h1>
-
-<p>Hier komt later veel meer informatie uit de database</p>
 
 
 
