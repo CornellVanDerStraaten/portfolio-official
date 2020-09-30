fcfcda3b22a55caabaf81cb7a7bae7431276ef29
Cornell van der Straaten <30183@ma-web.nl>
2020-09-30 21:02:34 +0200

    fhn

--- private/views/homepage.php
+++ private/views/homepage.php
@@ -7,11 +7,31 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body class="website-wrapper" style="background-image: url(<?php echo site_url('/images/') . 'cartographer.png'?>);">
-	<img class="website-logo">
+	<div class="page-top-bar">
+		<img class="website-logo" src="<?php echo site_url('/images/') . 'logo.svg'?>">
+	</div>
+	
 	<div class="home-red-square" style="background-image: url(<?php echo site_url('/images/') . 'cartographer.png'?>);">
-		<h1 class="header-text">Web & Software <br> Developer</h1>
+		<h1 class="header-text">
+		<span class="header-text-outside">W</span>
+		<span class="header-text-inside">e</span>	
+		<span class="header-text-inside">b</span>	
+		<span>&nbsp;</span>
+		<span class="header-text-inside">&</span>
+		<span>&nbsp;</span>
+		<span class="header-text-inside">S</span>
+		<span class="header-text-inside">o</span>
+		<span class="header-text-inside">f</span>
+		<span class="header-text-inside">t</span>
+		<span class="header-text-inside">w</span>
+		<span class="header-text-inside">a</span>
+		<span class="header-text-outside">r</span>
+		<span class="header-text-outside">e</span>
+		<br> <span class="header-text-dev">Developer<span></h1>
 		<h2 class="header-subtext">Cornell van der Straaten</h2>
 	</div>
+	<div class="inverted-bar" data-content="Lorem ipsum dolor sit amet" style="background-image: url(<?php echo site_url('/images/') . 'cartographer.png'?>);"></div>
+
 		
 	
 </body>
